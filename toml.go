3c5179d72a64301fbdd21317eb4ae118b8de1b5c
rorycl <rorycl>
2022-04-10 13:45:44 +0100

    test: setup testdata

--- toml.go
+++ toml.go
@@ -38,7 +38,7 @@ func LoadToml(file string) (Tables, error) {
 }
 
 func main() {
-	lt, err := LoadToml("settings2.toml")
+	lt, err := LoadToml("settings.toml")
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)

021af3d34b99edf461594a8169793d000b433d98
rorycl <rorycl>
2022-04-11 14:53:24 +0100

    tests: change exemplar toml test, associated settings testdata

--- toml_test.go
+++ toml_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"strings"
 	"testing"
 )
 
@@ -13,24 +14,23 @@ func TestTomlSettings(t *testing.T) {
 
 	// {Title:example settings file for gopg-anonymiser Tables:map[messages:{TableName:public.messages Filters:[{Column:all Filter:delete Source:}]} users:{TableName:public.users Filters:[{Column:name Filter:file_replace Source:/tmp/newnames.txt} {Column:password Filter:string_replace Source:this is a password}]}]} toml_test.go:17: {Title:example settings file for gopg-anonymiser Tables:map[messages:{TableName:public.messages Filters:[{Column:all Filter:delete Source:}]} users:{TableName:public.users Filters:[{Column:name Filter:file_replace Source:/tmp/newnames.txt} {Column:password Filter:string_replace Source:this is a password}]}]}
 
-	if lt.Title != "example settings file for gopg-anonymiser" {
+	if lt.Title != "test settings file for gopg-anonymiser" {
 		t.Errorf("Title incorrect")
 	}
-	if len(lt.Tables["messages"].Filters) != 1 {
-		t.Errorf("the messages table should have one filter")
+	if len(lt.Tables["events"].Filters) != 1 {
+		t.Errorf("the events table should have one filter")
 	}
-	if len(lt.Tables["users"].Filters) != 2 {
-		t.Errorf("the users table should have two filters")
+	if len(lt.Tables["users"].Filters) != 3 {
+		t.Errorf("the users table should have three filters")
 	}
-
 	if lt.Tables["users"].Filters[1].Column != "password" {
-		t.Errorf("the second users column should be 'password'")
+		t.Errorf("the second users target column should be 'password'")
 	}
 	if lt.Tables["users"].Filters[1].Filter != "string_replace" {
 		t.Errorf("the second users filter should be string_replace")
 	}
-	if lt.Tables["users"].Filters[1].Source != "this is a password" {
-		t.Errorf("the second users source should be 'this is a password'")
+	if !strings.Contains(lt.Tables["users"].Filters[1].Source, "$2a$06$.wHg4l7") {
+		t.Errorf("the second users source should start '$2a$06$.wHg4l7'")
 	}
 
 	t.Logf("%+v\n", lt)

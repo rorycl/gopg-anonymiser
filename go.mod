e008605a026811fbbf61d6eb413a2e98d8c8bf0f
rorycl <rorycl>
2022-04-12 10:24:58 +0100

    general: update main to take command line arguments

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module campbell-lange.net/gopg-anonymise
 
 go 1.17
 
-require github.com/BurntSushi/toml v1.1.0 // indirect
+require (
+	github.com/BurntSushi/toml v1.1.0 // indirect
+	github.com/jessevdk/go-flags v1.5.0 // indirect
+	golang.org/x/sys v0.0.0-20210320140829-1e4c9ba3b0c4 // indirect
+)
--- main.go
+++ main.go
@@ -1,16 +1,59 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"io"
 	"os"
+
+	flags "github.com/jessevdk/go-flags"
 )
+
+var usage = `
+
+anonymise a postgresql dump file using a toml settings file setting out
+the deletion, string_replace and file_replace filters to use.
+
+`
+
+// Options set the programme flag options
+type Options struct {
+	Settings string `short:"s" long:"settings" required:"true" description:"settings toml file"`
+	Input    string `short:"i" long:"input" required:"true" description:"input file or - for stdin"`
+	Test     bool   `short:"t" long:"testmode" description:"show only changed lines for testing"`
+	Args     struct {
+		Output string `description:"output file or - for stdout"`
+	} `positional-args:"yes" required:"yes"`
+}
 
 func main() {
 
-	output := os.Stdout
-	err := Anonymise("testdata/pg_dump.sql", "testdata/settings.toml", output, false)
+	var options Options
+	var parser = flags.NewParser(&options, flags.Default)
+	parser.Usage = fmt.Sprintf(usage)
+
+	if _, err := parser.Parse(); err != nil {
+		os.Exit(1)
+	}
+
+	// open stdout or file for writing
+	var output io.Writer
+	var err error
+	if options.Args.Output == "-" {
+		output = os.Stdout
+	} else {
+		filer, err := os.Create(options.Args.Output)
+		if err != nil {
+			fmt.Printf("Could not create file %s, %s", output, err)
+			os.Exit(1)
+		}
+		defer filer.Close()
+		output = filer
+	}
+
+	// run anonymiser
+	err = Anonymise(options.Input, options.Settings, output, options.Test)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Printf("Anonymisation error: %w\n", err)
 	}
 
 }

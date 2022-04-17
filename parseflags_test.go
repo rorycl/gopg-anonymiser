a6a4767e49e951aadfb294933e91054bb0419811
rorycl <rorycl>
2022-04-17 14:19:03 +0100

    parseflags: add tests

--- parseflags_test.go
+++ parseflags_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io/ioutil"
 	"os"
 	"testing"
 )
@@ -20,6 +21,32 @@ func TestFlagParsingFail(t *testing.T) {
 	_, err := parseFlags()
 	if err == nil {
 		t.Errorf("flag parsing should have failed %s", err)
+	}
+	t.Log(err)
+}
+
+func TestFlagParsingOutputStdout(t *testing.T) {
+
+	os.Args = []string{"prog", "-s", "settings.toml", "-o", "-"}
+	_, err := parseFlags()
+	if err != nil {
+		t.Error("stdout output file should not fail")
+	}
+	t.Log(err)
+}
+
+func TestFlagParsingOutputFile(t *testing.T) {
+
+	outFile, err := ioutil.TempFile("/tmp/", "out_")
+	if err != nil {
+		t.Error("could not make output tempfile")
+	}
+	defer os.Remove(outFile.Name())
+
+	os.Args = []string{"prog", "-s", "settings.toml", "-o", outFile.Name()}
+	_, err = parseFlags()
+	if err != nil {
+		t.Error("temporary file output should not fail")
 	}
 	t.Log(err)
 }

4c3e2025800205d4b62f39d7a86694503ee4660c
rorycl <rorycl>
2022-04-23 15:33:50 +0100

    anonymise: remove loading of filters from anonymisation; alter tests

--- anonymise.go
+++ anonymise.go
@@ -2,90 +2,10 @@ package main
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
 	"io"
-	"os"
 	"strings"
 )
-
-// loadFilters loads a set of table transformation filters for a dump
-// table from settings
-func loadFilters(settings Settings, dt *DumpTable) ([]RowFilterer, error) {
-
-	rfs := []RowFilterer{}
-
-	// retrieve settings for this dump table else error
-	var filters []Filter
-	for tableName, tableFilters := range settings {
-		if tableName == dt.TableName {
-			filters = tableFilters
-			break
-		}
-	}
-	if len(filters) == 0 {
-		return rfs, fmt.Errorf("table '%s' could not be found in settings", dt.TableName)
-	}
-
-	// load filters
-	for _, f := range filters {
-
-		switch f.Filter {
-		case "delete":
-			filter, _ := NewDeleteFilter()
-			rfs = append(rfs, filter)
-
-		case "uuid":
-			filter, err := NewUUIDFilter(f.Columns, f.If, f.NotIf)
-			// fixme
-			if err != nil {
-				return rfs, fmt.Errorf("uuid filter error: %w", err)
-			}
-			rfs = append(rfs, filter)
-
-		case "string replace":
-			if len(f.Columns) < 1 {
-				return rfs, errors.New("string replace filter: must provide at lease one column")
-			}
-			if len(f.Columns) != len(f.Replacements) {
-				return rfs, errors.New("string replace filter: column length != replacement length")
-			}
-			filter, err := NewReplaceFilter(
-				f.Columns,
-				f.Replacements,
-				f.If,
-				f.NotIf,
-			)
-			if err != nil {
-				return rfs, fmt.Errorf("source error for string replace: %w", err)
-			}
-			rfs = append(rfs, filter)
-
-		case "file replace":
-			if len(f.Columns) < 1 {
-				return rfs, errors.New("file replace: must provide at lease one column")
-			}
-			filer, err := os.Open(f.Source)
-			if err != nil {
-				return rfs, fmt.Errorf("file replace filter error: %w", err)
-			}
-			filter, err := NewFileFilter(
-				f.Columns,
-				filer,
-				f.If,
-				f.NotIf,
-			)
-			if err != nil {
-				return rfs, fmt.Errorf("source error for file error: %w", err)
-			}
-			rfs = append(rfs, filter)
-
-		default:
-			return rfs, fmt.Errorf("filter type %s not known", f.Filter)
-		}
-	}
-	return rfs, nil
-}
 
 // anonArgs is the Anonymise function signature
 type anonArgs struct {
@@ -108,14 +28,11 @@ func Anonymise(args anonArgs) error {
 		return fmt.Errorf("settings load error %s", err)
 	}
 
-	interestingTables := []string{}
-	for tableName := range settings {
-		interestingTables = append(interestingTables, tableName)
-	}
+	// load filters
+	tableFilters, err := loadFilters(settings)
 
 	// init a dump table and related filters
 	dt := new(DumpTable)
-	dtFilters := []RowFilterer{}
 
 	scanner := bufio.NewScanner(args.dumpFile)
 
@@ -127,7 +44,7 @@ func Anonymise(args anonArgs) error {
 		if !dt.Inited() {
 
 			// try to init dt
-			dt, err = NewDumpTable(t, interestingTables)
+			dt, err = NewDumpTable(t, tableFilters)
 			switch err {
 			case ErrNoDumpTable:
 			case ErrNotInterestingTable:
@@ -136,13 +53,9 @@ func Anonymise(args anonArgs) error {
 				return fmt.Errorf("Error parsing line %s", t)
 			}
 
-			// load filters if dt inited
+			// re-initialise in-dump line numbers if the dumptable is
+			// now initalised
 			if dt.Inited() {
-				dtFilters, err = loadFilters(settings, dt)
-				if err != nil {
-					return fmt.Errorf("loading filters failed %w", err)
-				}
-				// re-initiliased in-dump line numbers
 				lineNo = 0
 			}
 
@@ -170,7 +83,7 @@ func Anonymise(args anonArgs) error {
 			row := NewRow(dt, columns, lineNo)
 
 			// filter rows
-			for _, f := range dtFilters {
+			for _, f := range dt.filters {
 				row, err = f.Filter(row)
 				if err != nil {
 					return fmt.Errorf("filter error on table %s: %w", dt.TableName, err)
